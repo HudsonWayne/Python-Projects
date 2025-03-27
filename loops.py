5a3ac4fa032c9c7d2ad173a248650e8d5deec686
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-03-27 09:23:00 +0200

    initial commit

--- loops.py
+++ loops.py
@@ -2,3 +2,17 @@
 # There are two primary types of loops= [1] while Loops and for loops.
 
 
+# For loops is used to iterate over a sequence(list,tuples,dict,string or range).
+
+# Syntax:
+# for variable in sequence:
+    # statements
+    
+    
+# fruits = ["a", "b", "c"]
+# for fruit in fruits:
+#     print(fruit)
+
+text = "Python"
+for char in text:
+    print(char)
