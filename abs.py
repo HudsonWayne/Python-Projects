9284f91a49116cd80098f10a2f9070f8af9b228b
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-03-27 09:01:57 +0200

    Doing loops

---
+++ abs.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def go(self):
+        pass 
+    
+    @abstractmethod
+    def stop(self):
+        pass
+    
+    
+class Car(Vehicle):
+    def go(self):
+        print("You drive the car")
+    
+car = Car()
+car.go()
+car.stop()
---
+++ loops.py
@@ -0,0 +1,4 @@
+#Loops are used to repeat a block of code multiple times.
+# There are two primary types of loops= [1] while Loops and for loops.
+
+
